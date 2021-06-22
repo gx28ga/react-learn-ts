fc309ba62b79ed8861c634c779f3d1867be7679f
guanxin <373103730@qq.com>
2021-06-22 21:17:51 +0800

    add new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
+import {Map, Marker } from 'react-bmapgl';
+interface MapClickEvent extends Event{
+  latlng: {
+    lng: number,
+    lat: number
+  }
+}
+const App:React.FC = () => {
+  const [point, setPoint] = useState<BMapGL.Point>(new BMapGL.Point(116.404449, 39.914889))
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='map' style={{ width: '600px', height: '600px', border: '1px solid #333'}} >
+      <Map center={new BMapGL.Point(116.404449, 39.914889)} zoom={12} onClick={(e: MapClickEvent) => {
+        // 点击百度地图添加点位
+        setPoint(new BMapGL.Point(e.latlng.lng, e.latlng.lat))
+      }}>
+        {/*<Marker position={point}  icon='simple_red' />*/}
+        {/*{ point && <Marker position={point}  icon='simple_red' />}*/}
+        {/*  http://huiyan.baidu.com/github/react-bmapgl/#/%E8%A6%86%E7%9B%96%E7%89%A9/Marker */}
+        {/*map *地图实例，来自父元素<Map>的继承，无需手动传入*/}
+      </Map>
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
